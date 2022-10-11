166a434076182ea79593ae6d0682e2da2d759f14
IamtheBurnt <115515982+KalebWeaver@users.noreply.github.com>
2022-10-11 09:14:04 -0500

    addition of posts and user models

--- index.js
+++ index.js
@@ -4,15 +4,30 @@ const { ApolloServer } = require('apollo-server')
 const gql = require('graphql-tag')
 const mongoose = require('mongoose')
 
+const Post = require('./models/Posts')
+
 const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
   type Query {
-    sayHi: String!
+    getPosts: [Post]
   }
 `
 
 const resolvers = {
   Query: {
-    sayHi: () => 'Hello World!',
+    async getPosts() {
+      try {
+        const posts = await Post.find()
+        return posts
+      } catch (err) {
+        throw new Error(err)
+      }
+    },
   },
 }
 
