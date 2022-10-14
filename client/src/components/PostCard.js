50db225cca3b76977d465cba02b6e2d2489a8e40
IamtheBurnt <115515982+KalebWeaver@users.noreply.github.com>
2022-10-14 12:00:18 -0500

    added buttons to card

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import moment from 'moment'
 import { Link } from 'react-router-dom'
-import { Card, Image } from 'semantic-ui-react'
+import { Card, Image, Button, Icon, Label } from 'semantic-ui-react'
 
 export default function PostCard({
   post: { body, createdAt, id, username, likeCount, commentCount, likes },
 }) {
+  function likePost() {
+    console.log('Like post')
+  }
+
+  function commentOnPost() {
+    console.log('Comment on post')
+  }
+
   return (
     <Card fluid>
       <Card.Content>
@@ -21,7 +29,22 @@ export default function PostCard({
         <Card.Description>{body}</Card.Description>
       </Card.Content>
       <Card.Content extra>
-        <p>Buttons Here</p>
+        <Button as="div" labelPosition="right" onClick={likePost}>
+          <Button color="teal" basic>
+            <Icon name="heart" />
+          </Button>
+          <Label basic color="teal" pointing="left">
+            {likeCount}
+          </Label>
+        </Button>
+        <Button as="div" labelPosition="right" onClick={commentOnPost}>
+          <Button color="blue" basic>
+            <Icon name="comments" />
+          </Button>
+          <Label basic color="blue" pointing="left">
+            {commentCount}
+          </Label>
+        </Button>
       </Card.Content>
     </Card>
   )
